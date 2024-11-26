52e13a473cfad635ccb144fd32c3d1ca7eee8f54
a13xbb <alex.blokh24@gmail.com>
2024-11-26 21:28:51 +0300

    nlogn rmq done

--- rmq_lca/main.cpp
+++ rmq_lca/main.cpp
@@ -5,36 +5,42 @@
 class SparseTable {
 private:
     std::vector<std::vector<int>> table;
-    std::vector<int> log;
+    std::vector<int> A;
 
 public:
     SparseTable(const std::vector<int>& arr) {
-        int n = arr.size();
+        A = arr;
+
+        int n = A.size();
         int max_log = log2(n) + 1;
 
         table.resize(n, std::vector<int>(max_log));
-        log.resize(n + 1);
 
         for (int i = 0; i < n; i++) {
-            table[i][0] = arr[i];
-        }
-        
-        log[1] = 0;
-        for (int i = 2; i < n; i++) {
-            log[i] = log[i / 2] + 1;
+            table[i][0] = i;
         }
 
         for (int j = 1; j < max_log; j++) {
             for (int i = 0; i + (1 << j) <= n; i++) {
-                table[i][j] = std::min(table[i][j - 1],
-                                       table[i + (1 << ( j- 1))][j - 1]);
+                if (A[table[i][j - 1]] <= A[table[i + (1 << (j- 1))][j - 1]]) {
+                    table[i][j] = table[i][j - 1];
+                } else {
+                    table[i][j] = table[i + (1 << (j- 1))][j - 1];
+                }
             }
         }
     }
 
     int rmq(int l, int r) {
+        if (l < 0 || r >= A.size()) {
+            std::cerr<<"Index out of range\n";
+            return -1;
+        }
         int j = std::__lg(r - l + 1);
-        return std::min(table[l][j], table[r - (1 << j) + 1][j]);
+        if (A[table[l][j]] <= A[table[r - (1 << j) + 1][j]]) {
+            return table[l][j];
+        }
+        return table[r - (1 << j) + 1][j];
     }
 };
 
@@ -44,5 +50,5 @@ int main() {
     std::vector<int> vec = {2, 8, 4, -1, 55, 3, -10};
 
     SparseTable st = SparseTable(vec);
-    std::cout<<st.rmq(4, 5)<<"\n";
+    std::cout<<st.rmq(0, 5)<<"\n";
 }
